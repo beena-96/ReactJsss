de80e7db383ec38cf031839561d36ef4fb3219c2
Beena Nailwal <beena.nailwal@omlogistics.co.in>
2024-08-04 20:51:24 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import FunctionClass from './FunctionClass';
 
 function App() {
   return (
     <div className="App">
      <h1>app file</h1>
+     <FunctionClass />
     </div>
   );
 }
---
+++ src/FunctionClass.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+// function FunctionClass() {
+//   return (
+//     <div>Function Component</div>
+//   )
+// }
+
+// export default FunctionClass
+
+
+import React, {Component} from "react";
+
+export default class FunctionClass extends Component {
+    render() {
+        return (
+            <h2>class component</h2>
+        )
+    }
+}
